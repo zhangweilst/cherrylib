d1ab3fb19aaa63481a265147e4b5fd16b3d5ba39
zhangwei <zhangwei@weida.com>
2016-08-12 18:08:57 +0800

    Add C# project

---
+++ attribute_csharp/Program.cs
@@ -0,0 +1,48 @@
+﻿// #define MY_CONDITION
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace attribute_csharp
+{
+    public class myClass
+    {
+        [Conditional("MY_CONDITION")]
+        public static void Message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    public class timePeriod
+    {
+        private double seconds;
+        public double hours
+        {
+            get { return seconds * 3600; }
+            set { seconds = hours * 3600; }
+        }
+    }
+    class Program
+    {
+        static void function1()
+        {
+            myClass.Message("In function 1.");
+            function2();
+        }
+        static void function2()
+        {
+            myClass.Message("In function 2.");
+        }
+        static void Main(string[] args)
+        {
+            myClass.Message("In function main.");
+            function1();
+            Console.ReadKey();
+        }
+    }
+}
